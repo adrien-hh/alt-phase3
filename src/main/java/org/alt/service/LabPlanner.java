4f836fe52fdd9d695591ae2140c14ff1787c7722
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-03 18:54:22 +0100

    feat: started a LabPlanner service that will contain all the logic

---
+++ src/main/java/org/alt/service/LabPlanner.java
@@ -0,0 +1,11 @@
+package org.alt.service;
+
+import org.alt.bo.dto.LabInput;
+import org.alt.bo.dto.LabOutput;
+
+public class LabPlanner {
+    public void planifyLab(LabInput input) {
+        // logique pure ici
+        System.out.println(input.toString());
+    }
+}
