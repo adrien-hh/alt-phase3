eb6652e3a5b7ef53fe2c322890600a95dd365e30
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-03 18:54:43 +0100

    feat: added JSON parsing in Main, reading files from /main/resources

--- src/main/java/org/alt/Main.java
+++ src/main/java/org/alt/Main.java
@@ -1,7 +1,28 @@
 package org.alt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.alt.bo.dto.LabInput;
+import org.alt.bo.dto.LabOutput;
+import org.alt.service.LabPlanner;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+
+        InputStream inputStream = Main.class
+                .getClassLoader()
+                .getResourceAsStream("example1-input.json");
+
+        LabInput input = mapper.readValue(inputStream, LabInput.class);
+
+        LabPlanner planner = new LabPlanner();
+        planner.planifyLab(input);
+        // LabOutput output = planner.planifyLab(input);
+
+        // mapper.writerWithDefaultPrettyPrinter(.writeValue(System.out, output);
+    }
 }
