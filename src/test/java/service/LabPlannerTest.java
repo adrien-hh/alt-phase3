8e5cab9abf5df3f51ace8b803dc9a0625c6c15f9
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-03 20:25:08 +0100

    refactor: parameterized test for examples, cleaner and simpler

--- src/test/java/service/LabPlannerTest.java
+++ src/test/java/service/LabPlannerTest.java
@@ -6,63 +6,26 @@ import org.alt.bo.dto.LabInput;
 import org.alt.bo.dto.LabOutput;
 import org.alt.service.LabPlanner;
 import org.alt.utils.JsonUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LabPlannerTest {
 
-    @Test
-    void example1_should_match_expected_output() throws Exception {
+    private final ObjectMapper mapper = JsonUtils.mapper();
+    private final LabPlanner planner = new LabPlanner();
 
-        ObjectMapper mapper = JsonUtils.mapper();
+    @ParameterizedTest
+    @ValueSource(strings = {"example1", "example2", "example3"})
+    void examples_should_match_expected_output(String name) throws Exception {
 
-        LabInput input = JsonUtils.read("example1-input.json", LabInput.class);
+        LabInput input = JsonUtils.read(name + "-input.json", LabInput.class);
+        LabOutput actualOutput = planner.planifyLab(input);
 
-        LabOutput actualOutput = new LabPlanner().planifyLab(input);
-
-        JsonNode expected = JsonUtils.readAsJsonNode("example1-expected.json");
+        JsonNode expected = JsonUtils.readAsJsonNode(name + "-expected.json");
         JsonNode actual = mapper.valueToTree(actualOutput);
 
-        // Canonicalize: reparse serialized form (normalise numbers/structure)
-        JsonNode expectedNorm = mapper.readTree(mapper.writeValueAsBytes(expected));
-        JsonNode actualNorm = mapper.readTree(mapper.writeValueAsBytes(actual));
-
-        assertEquals(expectedNorm, actualNorm);
-    }
-
-    @Test
-    void example2_should_match_expected_output() throws Exception {
-
-        ObjectMapper mapper = JsonUtils.mapper();
-
-        LabInput input = JsonUtils.read("example2-input.json", LabInput.class);
-
-        LabOutput actualOutput = new LabPlanner().planifyLab(input);
-
-        JsonNode expected = JsonUtils.readAsJsonNode("example2-expected.json");
-        JsonNode actual = mapper.valueToTree(actualOutput);
-
-        // Canonicalize: reparse serialized form (normalise numbers/structure)
-        JsonNode expectedNorm = mapper.readTree(mapper.writeValueAsBytes(expected));
-        JsonNode actualNorm = mapper.readTree(mapper.writeValueAsBytes(actual));
-
-        assertEquals(expectedNorm, actualNorm);
-    }
-
-    @Test
-    void example3_should_match_expected_output() throws Exception {
-
-        ObjectMapper mapper = JsonUtils.mapper();
-
-        LabInput input = JsonUtils.read("example3-input.json", LabInput.class);
-
-        LabOutput actualOutput = new LabPlanner().planifyLab(input);
-
-        JsonNode expected = JsonUtils.readAsJsonNode("example3-expected.json");
-        JsonNode actual = mapper.valueToTree(actualOutput);
-
-        // Canonicalize: reparse serialized form (normalise numbers/structure)
         JsonNode expectedNorm = mapper.readTree(mapper.writeValueAsBytes(expected));
         JsonNode actualNorm = mapper.readTree(mapper.writeValueAsBytes(actual));
 
