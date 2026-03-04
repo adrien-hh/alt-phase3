fa28c32fea4af19ada05c7c26a200a8d68edf207
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-04 10:29:18 +0100

    feat: added TimeWindow record and Deserializer to manage Equipment maintenanceWindow or technician lunchBreak

---
+++ src/main/java/org/alt/bo/input/intermediate/TimeWindow.java
@@ -0,0 +1,9 @@
+package org.alt.bo.input.intermediate;
+
+import java.time.LocalTime;
+
+public record TimeWindow(LocalTime start, LocalTime end) {
+    public boolean overlaps(LocalTime from, LocalTime to) {
+        return from.isBefore(end) && to.isAfter(start);
+    }
+}
